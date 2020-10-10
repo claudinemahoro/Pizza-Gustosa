b26408aea12154e5251266c4122fd78ae3410c8f
claudinemahoro <claudinemah3@gmail.com>
2020-10-10 23:23:47 +0200

    add and style the buttons of checking cost

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+// alert("hi there");
+// // $("#seePrice").placeholder="hi";
+// $("#cost").click(function(){
+//   document.getElementById("seePrice").textContent = "price";
+// });
+// $("#totalCost").click(function(){
+//   document.getElementById("seeTotal").textContent = "Total";
+// });
+// });
